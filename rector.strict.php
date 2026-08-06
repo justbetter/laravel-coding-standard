93b6ea75313666ff07b0da41c1d428a850a25e47
indykoning <15870933+indykoning@users.noreply.github.com>
2026-08-06 14:38:06 +0200

    Add skip for ReplaceTestAnnotationWithPrefixedFunctionRector

--- rector.strict.php
+++ rector.strict.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Configuration\RectorConfigBuilder;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
 
 /** @var RectorConfigBuilder $rectorConfig */
 $rectorConfig = require __DIR__ . '/rector.php';
@@ -18,4 +19,6 @@ return $rectorConfig
         earlyReturn: true,
         carbon: true,
         phpunitCodeQuality: true,
-    );
+    )->withSkip([
+        ReplaceTestAnnotationWithPrefixedFunctionRector::class // prevent replacing @test with function testxxxx https://getrector.com/rule-detail/replace-test-annotation-with-prefixed-function-rector
+    ]);
